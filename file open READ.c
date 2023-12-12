2b7f5d7cbbbc009cfb49af039d84fab785a0c633
manojbandal <149452477+manojbandal@users.noreply.github.com>
2023-12-12 23:03:52 +0530

    Create file open READ.c

---
+++ file open READ.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAXCHAR 1000
+int main() {
+ FILE *fp;
+ char str[MAXCHAR];
+ char* filename = "E:\mk.txt";
+ fp = fopen(filename, "r");
+ if (fp == NULL){
+ printf("Could not open file %s",filename);
+ return 1;
+ }
+ while (fgets(str, MAXCHAR, fp) != NULL)
+ printf("%s", str);
+ fclose(fp);
+ return 0;
+}
